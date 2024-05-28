310b48bf59742866074e4e912d031eeeb34a18a4
Fauzan Rachmatullah <20058967@btpn.com>
2024-05-28 12:37:53 +0700

    initial commit

---
+++ src/main/java/com/example/toko_kacamata/TokoKacamataApplication.java
@@ -0,0 +1,13 @@
+package com.example.toko_kacamata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoKacamataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoKacamataApplication.class, args);
+	}
+
+}
